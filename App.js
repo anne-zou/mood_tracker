803194866665e8da3ca14ac527ac3258e3ffbd05
Anne Zou <annezou97@gmail.com>
2023-03-16 00:53:15 -0700

    clean up code

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import * as React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Layout from "./components/Layout";
-
-const styles = StyleSheet.create({});
 
 export default function App() {
   return <Layout />;
