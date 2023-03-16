d24d76f8507d4b68ce0dd011d46134eaef903a37
Anne Zou <annezou97@gmail.com>
2023-03-16 01:26:17 -0700

    use realm and redux

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
-import * as React from "react";
+import React from "react";
 import Layout from "./components/Layout";
+import { Provider } from "react-redux";
+import store from "./store/redux_store";
+import Realm from "realm";
+import {
+  MoodEntrySchema,
+  MOOD_ENTRY_SCHEMA_KEY,
+} from "./database/realm_schemas";
+
+const realmConfig = {
+  path: "./database/data.realm",
+  schema: [MoodEntrySchema],
+  schemaVersion: 0,
+};
 
 export default function App() {
-  return <Layout />;
+  useEffect(async () => {
+    await Realm.open(realmConfig);
+  }, []);
+  return <Provider store={store}>
+    <Layout />
+  </Provider>;
 }
